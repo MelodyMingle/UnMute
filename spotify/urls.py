dd2a6a533e4d54a664d7a3e453d69e8bfa6e61b9
Chris Acosta <chrisacosta12345@gmail.com>
2024-03-13 15:47:34 -0700

    added spotify functionality

--- spotify/urls.py
+++ spotify/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import AuthURL, spotify_callback, IsAuthenticated, SpotifySearch
+from .views import AuthURL, spotify_callback, IsAuthenticated, SpotifySearch, SpotifySearchPlaylist
 
 urlpatterns = [
     path('get-auth-url', AuthURL.as_view()),
     path('callback', spotify_callback),
     path('is-authenticated', IsAuthenticated.as_view()),
-    path('search', SpotifySearch.as_view({'get':'list'}), name='spotify_list')
+    path('search', SpotifySearch.as_view({'get':'list'}), name='spotify_list'),
+    path('playlists', SpotifySearchPlaylist.as_view({'get':'list'}), name='spotify_playlist')
 ]
--- spotify/views.py
+++ spotify/views.py
@@ -6,15 +6,15 @@ from requests import Request, post
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
-from .util import update_or_create_user_tokens, is_spotify_authenticated, get_user_tokens
+from .util import update_or_create_user_tokens, is_spotify_authenticated, get_user_tokens, refresh_spotify_token
 from .models import SpotifyToken
 import requests
-from django.http import JsonResponse
+
 
 
 class AuthURL(APIView):
     def get(self, request, fornat=None):
-        scopes = 'user-read-playback-state user-modify-playback-state user-read-currently-playing'
+        scopes = 'user-read-playback-state user-modify-playback-state user-read-currently-playing playlist-read-private'
 
         url = Request('GET', 'https://accounts.spotify.com/authorize', params={
             'scope': scopes,
@@ -22,45 +22,38 @@ class AuthURL(APIView):
             'redirect_uri': REDIRECT_URI,
             'client_id': CLIENT_ID
         }).prepare().url
-        
+
         return redirect(url)
 
 
 def spotify_callback(request, format=None):
+    print('SPOTIFY CALLBACK WORKS!!!!')
     code = request.GET.get('code')
+    print('I GOT THE DMAN CODE', code)
     error = request.GET.get('error')
 
-    if error:
-        return JsonResponse({'error': 'Authorization failed'}, status=400)
+    response = post('https://accounts.spotify.com/api/token', data={
+        'grant_type': 'authorization_code',
+        'code': code,
+        'redirect_uri': REDIRECT_URI,
+        'client_id': CLIENT_ID,
+        'client_secret': CLIENT_SECRET
+    }).json()
+    print('spotify callback!!', response)
 
-    if code:
-        response = post('https://accounts.spotify.com/api/token', data={
-            'grant_type': 'authorization_code',
-            'code': code,
-            'redirect_uri': REDIRECT_URI,
-            'client_id': CLIENT_ID,
-            'client_secret': CLIENT_SECRET
-        }).json()
-        
-        access_token = response.get('access_token')
-        token_type = response.get('token_type')
-        refresh_token = response.get('refresh_token')
-        expires_in = response.get('expires_in')
+    access_token = response.get('access_token')
+    token_type = response.get('token_type')
+    refresh_token = response.get('refresh_token')
+    expires_in = response.get('expires_in')
+    error = response.get('error')
+    
+    if not request.session.exists(request.session.session_key):
+        request.session.create()
 
-        # Ensure we have all necessary data before proceeding
-        if not access_token or not token_type or not refresh_token or expires_in is None:
-            return JsonResponse({'error': 'Invalid token data received from Spotify'}, status=400)
+    update_or_create_user_tokens(
+        request.session.session_key, access_token, token_type, expires_in, refresh_token)
 
-        if not request.session.exists(request.session.session_key):
-            request.session.create()
-
-        update_or_create_user_tokens(
-            request.session.session_key, access_token, token_type, expires_in, refresh_token)
-        
-        # Assuming 'home' is a valid named URL in your Django project
-        return redirect('home')
-    else:
-        return JsonResponse({'error': 'Authorization code not provided'}, status=400)
+    return redirect('/')
 
 
 class IsAuthenticated(APIView):
@@ -73,20 +66,18 @@ class SpotifySearch(GenericViewSet):
     permission_classes = (AllowAny,)
 
     def list(self, request):
-        # Assuming the session_id can be derived from the request in some manner,
-        # for example, from the user's session. Adjust this according to your authentication setup.
-        session_id = request.session.session_key
+        
+        session_id = self.request.session.session_key
+        print('session id', request.session)
 
-        # Ensure the user is authenticated with Spotify and the token is refreshed if necessary.
-        if not is_spotify_authenticated(session_id):
-            return Response({'error': 'Authentication with Spotify failed or token expired'}, status=status.HTTP_401_UNAUTHORIZED)
-
-        # Now we can safely assume the token is valid
+        if not is_spotify_authenticated(session_id) and not refresh_spotify_token(session_id):
+            return Response({'error': 'Failed to authenticate with Spotify'}, status=status.HTTP_401_UNAUTHORIZED)
+        
         token = get_user_tokens(session_id)
         if token is None:
-            return Response({'error': 'Spotify token not found'}, status=status.HTTP_404_NOT_FOUND)
+            return Response({'error': 'No Spotify token found'}, status=status.HTTP_404_NOT_FOUND)
 
-        access_token = SpotifyToken.objects.filter(user=session_id)
+        access_token = token.access_token
         headers = {"Authorization": f"Bearer {access_token}"}
         query = request.query_params.get("query")
         type_of_search = "track"
@@ -98,9 +89,31 @@ class SpotifySearch(GenericViewSet):
 
         return Response(response.json(), status=status.HTTP_200_OK)
 
+class SpotifySearchPlaylist(GenericViewSet):
+    permission_classes = (AllowAny,)
+    
+    def list(self, request):
+        
+        session_id = self.request.session.session_key
+    
+        if not is_spotify_authenticated(session_id) and not refresh_spotify_token(session_id):
+            return Response({'error': 'Failed to authenticate with Spotify'}, status=status.HTTP_401_UNAUTHORIZED)
+        
+        token = get_user_tokens(session_id)
+        if token is None:
+            return Response({'error': 'No Spotify token found'}, status=status.HTTP_404_NOT_FOUND)
 
-
-
+        access_token = token.access_token
+        headers = {"Authorization": f"Bearer {access_token}"}
+        query = {}
+        limit = 20
+        offset = 0
+        params = {'q': query,}
+        url = "https://api.spotify.com/v1/me/playlists"
+        response = requests.get(url, headers=headers, params=params)
+        
+        return Response(response.json(), status=status.HTTP_200_OK)
+        
 
 
 
