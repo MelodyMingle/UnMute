61427327bb6c6a17027ad63c5a75b52abdcf2820
Andrea Riley <andreariley@me.com>
2024-03-14 14:31:00 -0400

    merging with chris

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,20 @@ module.exports = {
       './node_modules/flowbite/**/*.js'
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#2C032C', // deep purple
+        'secondary': '#691370', // mid purple
+        'tertiary': '#DCD0C4', // cream
+        'quaternary': '#C922AE', // pink
+        'quinary': '#EB9A2F', // orange
+        'senary': '#3F0140', // dark purple
+        'septenary': '#FFA500',
+        'octonary': '#FFD700',
+        'nonary': '#FFA500',
+        'denary': '#FFD700',
+      },
+    },
   },
   plugins: [
     require('flowbite/plugin')
